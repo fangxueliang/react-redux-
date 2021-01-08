c63189693150bd2a5aa5af44d1b946856b421173
fangxl <fxl19950129@163.com>
2021-01-08 18:37:48 +0800

    练习2

--- src/Component/StatelessUI/homeUI/MessageUI.js
+++ src/Component/StatelessUI/homeUI/MessageUI.js
@@ -1,10 +1,30 @@
-import React, { useContext } from 'react';
+import React, { useContext, useReducer, useEffect, useState } from 'react';
 import { MESSAGE_COMMON } from "../../Home/HomeComponent/homeComponent";
+import { Button, List } from "antd";
+import reducer from "../../../Store/reducer";
 
 const MessageUI = () => {
     const { message } = useContext(MESSAGE_COMMON);
+    const [arr, setArr] = useState([])
+    const [state, dispatch] = useReducer(reducer, {})//相当于connect 连接和redux的关系 并且取到数据
+    useEffect(() => {
+        setArr([{ name: 'xiaofang' }])
+    }, [])
     return (<div>
         {message}
+        <Button onClick={() => dispatch({ type: 'MENU' })}>点击调用reducer{console.log(state)}</Button>
+        <Button onClick={() => setArr([1, 2, 3, 4])}>点击调用reducer{console.log(state)}</Button>
+        <List
+            className="demo-loadmore-list"
+            itemLayout="horizontal"
+            dataSource={state.nameStatistical}
+            renderItem={item => (
+                <List.Item>
+                    {item.title}
+                </List.Item>
+            )}
+        />
+        {arr.map((item, index) => (<span key={index}>{item.name}{console.log(item)}</span>))}
     </div>)
 }
 export default MessageUI
